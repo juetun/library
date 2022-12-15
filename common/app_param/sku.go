f697166ceb27ac465691767809ac0f4b9a1e5911
zhaocj2009 <zhaocj2009@126.com>
2022-12-15 12:39:59 +0800

    12

---
+++ common/app_param/sku.go
@@ -0,0 +1,20 @@
+package app_param
+const (
+	SkuDataShop                = "sku_shop"         //店铺信息
+	SkuDataBrand               = "sku_brand"        //店铺品牌
+	SkuDataShopExt             = "sku_shop_ext"     //店铺扩展信息
+	SkuDataShopNotice          = "sku_shop_notice"  //店铺公告
+	SkuDataTypeProduct         = "sku_product"      //商品信息
+	SkuDataTypeProductDesc     = "sku_product_desc" //商品描述
+	SkuDataTypeStock           = "sku_stock"        //库存
+	SkuDataTypeGift            = "sku_gift"         //赠品
+	SkuDataTypeFreightTemplate = "sku_freight_gift" //运费模板
+
+	SkuUploadSkuImg          = "sku_img"         // SKU图片
+	SkuSpuUploadImgThumbnail = "spu_img_thumb"   // 商品缩略图
+	SkuSpuUploadImgPic       = "spu_img_li"      // 商品图片
+	SkuSpuUploadBrandImg     = "spu_brand_img"   // 品牌logo
+	SkuSpuUploadDescription  = "spu_upload_desc" // 商品详情的信息
+	SkuSpuUploadVideo        = "spu_video"       // 上传视频视频地址
+	SkuSpuShopLogo           = "spu_shop_logo"   // 店铺LOGO
+)
---
+++ common/app_param/spu.go
@@ -0,0 +1,24 @@
+package app_param
+
+const (
+	SpuDataShop                = "spu_shop"         // 店铺信息
+	SpuDataShopExt             = "spu_shop_ext"     // 店铺扩展信息
+	SpuDataShopNotice          = "spu_shop_notice"  // 店铺公告信息
+	SpuDataBrand               = "spu_brand"        // 品牌信息
+	SpuDataTypeProductDesc     = "spu_product_desc" // 商品描述
+	SpuDataTypeSKus            = "spu_sku"          // SKU信息
+	SpuDataTypeSKusGift        = "spu_gifts"        // 赠品信息
+	SpuDataTypeSKusStock       = "spu_sku_stock"    // sku库存
+	SpuDataTypeSKusProperty    = "spu_sku_property" // sku属性
+	SpuDataTypeFreightTemplate = "spu_freight_gift" //运费模板
+
+	/*****************************上传信息获取**********************************/
+	//SpuUploadAll          = "spu_img"         // 商品图片获取所有图片 (包括SpuImg前缀的属性如:SpuUploadImgThumbnail,SpuUploadImgPic,SpuSkuImg,SpuUploadDescription）
+	SpuUploadImgThumbnail = "spu_img_thumb"   // 商品缩略图
+	SpuUploadImgPic       = "spu_img_li"      // 商品图片
+	SpuUploadSkuImg       = "spu_sku_img"     // SKU图片
+	SpuUploadBrandImg     = "spu_brand_img"   // 品牌logo
+	SpuUploadDescription  = "spu_upload_desc" // 商品详情的信息
+	SpuUploadVideo        = "spu_video"       // 上传视频视频地址
+	SpuShopLogo           = "spu_shop_logo"   // 店铺LOGO
+)
