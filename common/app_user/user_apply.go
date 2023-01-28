3ab437ff1f4244865437580b6e7712294f9a05fd
changjiang <zhaocj2009@126.com>
2023-01-28 22:04:56 +0800

    12

---
+++ common/app_user/user_apply.go
@@ -0,0 +1,64 @@
+package app_user
+
+import "github.com/juetun/base-wrapper/lib/base"
+
+const (
+	UpdateBatchTypeInfo        = "shop_info"    //店铺信息
+	UpdateBatchTypeBrand       = "shop_brand"   //店铺品牌
+	UpdateBatchTypeCate        = "shop_cate"    //店铺类目
+	UpdateBatchTypeUserProfile = "user_profile" //用户资料
+)
+const (
+	UserApplyStatusUsing   uint8 = iota + 1 //使用中
+	UserApplyStatusInvalid                  //失效
+	UserApplyStatusFailure                  //审核失败
+	UserApplyStatusInit                     //编辑中
+	UserApplyStatusSubmit                   //提交审核
+)
+
+var (
+	SliceUpdateBatchType = base.ModelItemOptions{
+		{
+			Value: UpdateBatchTypeInfo,
+			Label: "店铺信息",
+		},
+		{
+			Value: UpdateBatchTypeBrand,
+			Label: "店铺品牌",
+		},
+		{
+			Value: UpdateBatchTypeCate,
+			Label: "店铺类目",
+		},
+		{
+			Value: UpdateBatchTypeUserProfile,
+			Label: "用户资料",
+		},
+	}
+)
+
+var (
+	//用户资料审核状态
+	SliceUserApplyStatus = base.ModelItemOptions{
+		{
+			Label: "使用中...",
+			Value: UserApplyStatusUsing,
+		},
+		{
+			Label: "已失效",
+			Value: UserApplyStatusInvalid,
+		},
+		{
+			Label: "审核失败",
+			Value: UserApplyStatusFailure,
+		},
+		{
+			Label: "编辑中...",
+			Value: UserApplyStatusInit,
+		},
+		{
+			Label: "审核中...",
+			Value: UserApplyStatusSubmit,
+		},
+	}
+)
