a1f694a34e505b12f2bd602b84f943d4b87eb62f
changjiang <zhaocj2009@126.com>
2023-01-29 13:25:03 +0800

    12

--- common/recommend/data_item.go
+++ common/recommend/data_item.go
@@ -2,17 +2,20 @@ package recommend
 
 type (
 	DataItem struct {
-		Title        string            `json:"title"`          //标题
-		PreTags      []*DataItemTag    `json:"pre_tags"`       //前缀标签
-		DataType     int8              `json:"data_type"`      //数据类型
-		DataTypeName string            `json:"data_type_name"` //数据类型名称
-		DataId       string            `json:"data_id"`        //数据ID
-		HaveVideo    bool              `json:"have_video"`     //是否有视频
-		Img          string            `json:"img"`            //头图
-		Price        string            `json:"price"`          //价格
-		DataValue    []*DataItemDetail `json:"data_value"`     //详情
-		SuffixTags   []*DataItemTag    `json:"suffix_tags"`    //后缀标签
-		ExtraMsg     string            `json:"extra_msg"`      // 携带的其他信息
+		Title         string            `json:"title,omitempty"`       //标题
+		PreTags       []*DataItemTag    `json:"pre_tags,omitempty"`    //前缀标签
+		DataType      int8              `json:"data_type"`             //数据类型
+		DataId        string            `json:"data_id"`               //数据ID
+		HaveVideo     bool              `json:"have_video,omitempty"`  //是否有视频
+		Img           string            `json:"img,omitempty"`         //头图
+		Price         string            `json:"price"`                 //价格
+		DataValue     []*DataItemDetail `json:"data_value,omitempty"`  //详情
+		SuffixTags    []*DataItemTag    `json:"suffix_tags,omitempty"` //后缀标签
+		ShowShop      bool              `json:"show_shop"`             //是否显示店铺名
+		ShopName      string            `json:"shop_name,omitempty"`   //店铺名称
+		CurrentAccUId int64             `json:"current_acc_uid"`       //获取数据的用户ID
+		ShopId        int64             `json:"shop_id,omitempty"`     //店铺ID
+		ExtraMsg      string            `json:"extra_msg"`             //携带的其他信息
 	}
 	DataItemTag struct {
 		Label string `json:"label"` //类型名称
