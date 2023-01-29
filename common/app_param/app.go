b798e40d9c1dbaddbd7b8715549a51b350ae6f8e
changjiang <zhaocj2009@126.com>
2023-01-29 21:58:53 +0800

    1

--- common/app_param/app.go
+++ common/app_param/app.go
@@ -3,20 +3,21 @@ package app_param
 import "github.com/juetun/base-wrapper/lib/base"
 
 const (
-	AppNameAdmin            = "admin-main"
-	AppNameUpload           = "api-upload"
-	AppNameExport           = "api-export"
-	AppNameUser             = "api-user"
-	AppNameTag              = "api-tag"
-	AppNameNotice           = "api-notice"
-	AppNameComment          = "api-comment"
-	AppNameChat             = "api-chat"
-	AppNameCar              = "api-car"
-	AppNameMall             = "api-mall"
-	AppNameMallOrder        = "api-order"
-	AppNameMallOrderComment = "api-ordercomment"
-	AppNameMallActivity     = "api-activity"
-	AppNameRecommend        = "api-recommend" //推荐
+	AppNameAdmin             = "admin-main"
+	AppNameUpload            = "api-upload"
+	AppNameExport            = "api-export"
+	AppNameUser              = "api-user"
+	AppNameTag               = "api-tag"
+	AppNameNotice            = "api-notice"
+	AppNameComment           = "api-comment"      //社交评论
+	AppNameChat              = "api-chat"         //聊天
+	AppNameCar               = "api-car"          //汽车
+	AppNameMall              = "api-mall"         //电商
+	AppNameMallOrder         = "api-order"        //订单
+	AppNameMallOrderComment  = "api-ordercomment" //订单评论
+	AppNameMallActivity      = "api-activity"     //电商活动
+	AppNameRecommend         = "api-recommend"    //推荐
+	AppNameSocialIntercourse = "api-sns"          //社交
 )
 
 //标签类型定义
@@ -84,6 +85,10 @@ var (
 		{
 			Label: "广告推荐",
 			Value: AppNameRecommend,
+		},
+		{
+			Label: "社交",
+			Value: AppNameSocialIntercourse,
 		},
 	}
 )
