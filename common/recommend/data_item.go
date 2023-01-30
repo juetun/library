7e3654e31f88027e7174d0517c66e676b619abcf
changjiang <zhaocj2009@126.com>
2023-01-30 10:17:33 +0800

    12

--- common/recommend/data_item.go
+++ common/recommend/data_item.go
@@ -9,23 +9,23 @@ const (
 
 type (
 	DataItem struct {
-		Title         string            `json:"title,omitempty"`       //标题
-		PreTags       []*DataItemTag    `json:"pre_tags,omitempty"`    //前缀标签
-		DataType      int8              `json:"data_type"`             //数据类型
-		DataId        string            `json:"data_id"`               //数据ID
-		Link          string            `json:"link"`                  //链接地址
-		HaveVideo     bool              `json:"have_video,omitempty"`  //是否有视频
-		Img           string            `json:"img,omitempty"`         //头图
-		Price         string            `json:"price"`                 //价格
-		DataValue     []*DataItemDetail `json:"data_value,omitempty"`  //详情
-		SuffixTags    []*DataItemTag    `json:"suffix_tags,omitempty"` //后缀标签
-		ShowShop      bool              `json:"show_shop"`             //是否显示店铺名
-		ShopName      string            `json:"shop_name,omitempty"`   //店铺名称
-		CurrentAccUId int64             `json:"current_acc_uid"`       //获取数据的用户ID
-		ShopId        int64             `json:"shop_id,omitempty"`     //店铺ID
-		ExtraMsg      string            `json:"extra_msg"`             //携带的其他信息
-		ShowType      string            `json:"show_type"`             //展示样式 默认card
-		Children      []*DataItem       `json:"children,omitempty"`    //子列表
+		Title         string                     `json:"title,omitempty"`       //标题
+		PreTags       []*DataItemTag             `json:"pre_tags,omitempty"`    //前缀标签
+		DataType      int8                       `json:"data_type"`             //数据类型
+		DataId        string                     `json:"data_id"`               //数据ID
+		Link          string                     `json:"link"`                  //链接地址
+		HaveVideo     bool                       `json:"have_video,omitempty"`  //是否有视频
+		Img           string                     `json:"img,omitempty"`         //头图
+		Price         string                     `json:"price"`                 //价格
+		DataValue     map[string]*DataItemDetail `json:"data_value,omitempty"`  //详情
+		SuffixTags    []*DataItemTag             `json:"suffix_tags,omitempty"` //后缀标签
+		ShowShop      bool                       `json:"show_shop"`             //是否显示店铺名
+		ShopName      string                     `json:"shop_name,omitempty"`   //店铺名称
+		CurrentAccUId int64                      `json:"current_acc_uid"`       //获取数据的用户ID
+		ShopId        int64                      `json:"shop_id,omitempty"`     //店铺ID
+		ExtraMsg      string                     `json:"extra_msg"`             //携带的其他信息
+		ShowType      string                     `json:"show_type"`             //展示样式 默认card
+		Children      []*DataItem                `json:"children,omitempty"`    //子列表
 	}
 	DataItemTag struct {
 		Label string `json:"label"` //类型名称
