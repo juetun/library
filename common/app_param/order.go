281ad39d49af41e0115ee9092bdc4a6fda6a1eb0
changjiang <zhaocj2009@126.com>
2023-02-11 13:12:56 +0800

    12

--- common/app_param/order.go
+++ common/app_param/order.go
@@ -11,9 +11,9 @@ type (
 		SaleType      uint8  `json:"sale_type" form:"sale_type"`           // 销售类型
 		SkuPrice      string `json:"sku_price" form:"sku_price"`           // SPU项目本次要支付的单价(定金预售定金金额或尾款金额 sku_price)
 		SkuSetPrice   string `json:"sku_set_price" form:"sku_set_price"`   // SPU项目本的单价
-		FreightTplId  string `json:"freight_tpl_id" form:"freight_tpl_id"` // 运费模板
+		FreightTplId  int64  `json:"freight_tpl_id" form:"freight_tpl_id"` // 运费模板
 		SubOrderId    string `json:"sub_order_id" form:"sub_order_id"`
-		FreightAmount string `json:"-" form:"-"` // 邮费
+		FreightAmount string `json:"freight_amount" form:"freight_amount"` // 邮费
 	}
 )
 
