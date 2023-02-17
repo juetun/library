fe36dbc96664f812114f3fd158bc8e907c61c3c6
changjiang <zhaocj2009@126.com>
2023-02-17 09:39:17 +0800

    12

--- common/recommend/data_recommend_product.go
+++ common/recommend/data_recommend_product.go
@@ -1,5 +1,8 @@
 package recommend
 
+import "github.com/juetun/base-wrapper/lib/base"
+
 type DataRecommendProduct struct {
-	ProductID string `json:"id" gorm:"column:id;"`
+	ProductID string          `json:"id" gorm:"column:id;"`
+	UpdatedAt base.TimeNormal `gorm:"column:updated_at;" json:"updated_at"`
 }
