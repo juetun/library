bb71d8a352cf3bb12bb8dc51a77dd09053d63c9e
changjiang <zhaocj2009@126.com>
2023-02-27 18:24:05 +0800

    12

--- common/app_param/sku.go
+++ common/app_param/sku.go
@@ -1,17 +1,18 @@
 package app_param
 
 const (
-	SkuDataTypeSku             = "sku_data"            //SKU信息
-	SkuDataShop                = "sku_shop"            //店铺信息
-	SkuDataBrand               = "sku_brand"           //店铺品牌
-	SkuDataShopExt             = "sku_shop_ext"        //店铺扩展信息
-	SkuDataShopNotice          = "sku_shop_notice"     //店铺公告
-	SkuDataTypeProduct         = "sku_product"         //商品信息
-	SkuDataTypeProductDesc     = "sku_product_desc"    //商品描述
-	SkuDataTypeStock           = "sku_stock"           //库存
-	SkuDataTypeProperty        = "sku_property"        //SKU属性
-	SkuDataTypeGift            = "sku_gift"            //赠品
-	SkuDataTypeFreightTemplate = "sku_freight_temp"    //运费模板
+	SkuDataTypeSku             = "sku_data"         //SKU信息
+	SkuDataTypeRelate          = "sku_relate"       //sku 属性关联
+	SkuDataShop                = "sku_shop"         //店铺信息
+	SkuDataBrand               = "sku_brand"        //店铺品牌
+	SkuDataShopExt             = "sku_shop_ext"     //店铺扩展信息
+	SkuDataShopNotice          = "sku_shop_notice"  //店铺公告
+	SkuDataTypeProduct         = "sku_product"      //商品信息
+	SkuDataTypeProductDesc     = "sku_product_desc" //商品描述
+	SkuDataTypeStock           = "sku_stock"        //库存
+	SkuDataTypeProperty        = "sku_property"     //SKU属性
+	SkuDataTypeGift            = "sku_gift"         //赠品
+	SkuDataTypeFreightTemplate = "sku_freight_temp" //运费模板
 
 	SkuUploadSkuImg          = "sku_img"         // SKU图片
 	SkuSpuUploadImgThumbnail = "spu_img_thumb"   // 商品缩略图
