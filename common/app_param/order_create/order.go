8de4a79d7f23a1670932bf39eab2d461400b2f62
changjiang <zhaocj2009@126.com>
2023-02-28 12:09:27 +0800

    12

--- common/app_param/order_create/order.go
+++ common/app_param/order_create/order.go
@@ -64,8 +64,6 @@ type (
 		SkuPropertyName string              `json:"sku_property_name"` //SKU属性名
 		DownPayment     string              `json:"down_payment"`      //定金
 		HaveVideo       bool                `json:"have_video"`        //是否有视频
-		MaxLimitNum     int64               `json:"max_limit_num"`     //最大添加数
-		MinLimitNum     int64               `json:"min_limit_num"`     //最下货物数
 		Mark            string              `json:"mark"`              //商品说明（如 比着加入有无车时降价多少）
 		MarkSystem      string              `json:"mark_system"`       //数据不合法 系统说明(系统使用，记录更详细不合法原因)
 		Checked         bool                `json:"checked"`           //是否选中
