63dd6e2c0e8d5631b607c96ae409c8c9b6ed660a
changjiang <zhaocj2009@126.com>
2023-03-02 22:47:20 +0800

    12

--- common/recommend/data_url.go
+++ common/recommend/data_url.go
@@ -11,6 +11,33 @@ import (
 func GetPageLink(urlValue *url.Values, dataType string) (res string, err error) {
 
 	switch dataType {
+	case AdDataDataTypeShop:
+		var (
+			stringValue  string
+			urlValue1    = url.Values{}
+			paramsDivide = "?"
+		)
+		if tmp, ok := plugins_lib.WebMap[dataType]; ok {
+			stringValue = fmt.Sprintf("//%s/#/pages/mall/shop/home/index", tmp)
+		} else {
+			stringValue = fmt.Sprintf("//127.0.0.1:10086/#/pages/mall/shop/home/index")
+		}
+		var (
+			dataSlice = strings.Split(stringValue, paramsDivide)
+		)
+		var l = len(dataSlice)
+		if l > 1 {
+			if urlValue1, err = url.ParseQuery(dataSlice[l-1]); err != nil {
+				return
+			}
+			preUrl := dataSlice[0 : l-1]
+			stringValue = strings.Join(preUrl, paramsDivide)
+			for key, value := range urlValue1 {
+				urlValue.Set(key, strings.Join(value, ""))
+			}
+		}
+		res = fmt.Sprintf("%s%s%s", stringValue, paramsDivide, urlValue.Encode())
+		return
 	case AdDataDataTypeSpu: //广告商品信息
 		var (
 			stringValue  string
