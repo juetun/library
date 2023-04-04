316d85c1f8f0b0209e50878580855b424b150930
changjiang <zhaocj2009@126.com>
2023-04-04 11:20:25 +0800

    12

--- common/app_param/upload_operate/upload_info.go
+++ common/app_param/upload_operate/upload_info.go
@@ -4,24 +4,24 @@ import "github.com/juetun/base-wrapper/lib/base"
 
 type (
 	UploadInfo struct {
-		Img      *UploadImage    `json:"img"`
+		//Img      *UploadImage    `json:"img"`
 		Video    *UploadVideo    `json:"video"`
 		Music    *UploadMusic    `json:"music"`
-		Material *UploadMaterial `json:"material"`
+		//Material *UploadMaterial `json:"material"`
 		File     *UploadFile     `json:"file"`
 	}
 	ResultMapUploadInfo struct {
-		Img      map[string]*UploadImage    `json:"img"`
+		//Img      map[string]*UploadImage    `json:"img"`
 		Video    map[string]*UploadVideo    `json:"video"`
 		Music    map[string]*UploadMusic    `json:"music"`
-		Material map[string]*UploadMaterial `json:"material"`
+		//Material map[string]*UploadMaterial `json:"material"`
 		File     map[string]*UploadFile     `json:"file"`
 	}
 	ArgUploadGetInfo struct {
-		ImgKeys   []string `json:"img_keys"`
+		//ImgKeys   []string `json:"img_keys"`
 		VideoKeys []string `json:"video_keys"`
 		MusicKey  []string `json:"music_key"`
-		Material  []string `json:"material"`
+		//Material  []string `json:"material"`
 		File      []string `json:"file"`
 	}
 )
@@ -33,10 +33,10 @@ func (r *ArgUploadGetInfo) Default(c *base.Context) (err error) {
 
 func NewArgUploadGetInfo() (res *ArgUploadGetInfo) {
 	res = &ArgUploadGetInfo{
-		ImgKeys:   make([]string, 0, 50),
+		//ImgKeys:   make([]string, 0, 50),
 		VideoKeys: make([]string, 0, 10),
 		MusicKey:  make([]string, 0, 10),
-		Material:  make([]string, 0, 50),
+		//Material:  make([]string, 0, 50),
 		File:      make([]string, 0, 50),
 	}
 	return
@@ -45,7 +45,9 @@ func NewArgUploadGetInfo() (res *ArgUploadGetInfo) {
 //判断数据是否为空
 func (r *ArgUploadGetInfo) IsNull() (isNull bool) {
 
-	if len(r.ImgKeys) == 0 && len(r.VideoKeys) == 0 && len(r.MusicKey) == 0 && len(r.Material) == 0 && len(r.File) == 0 {
+	if len(r.ImgKeys) == 0 && len(r.VideoKeys) == 0 && len(r.MusicKey) == 0 &&
+		//len(r.Material) == 0 &&
+		len(r.File) == 0 {
 		isNull = true
 		return
 	}
@@ -54,10 +56,10 @@ func (r *ArgUploadGetInfo) IsNull() (isNull bool) {
 
 func NewResultMapUploadInfo() (res *ResultMapUploadInfo) {
 	res = &ResultMapUploadInfo{
-		Img:      make(map[string]*UploadImage, 50),
+		//Img:      make(map[string]*UploadImage, 50),
 		Video:    make(map[string]*UploadVideo, 10),
 		Music:    make(map[string]*UploadMusic, 10),
-		Material: make(map[string]*UploadMaterial, 50),
+		//Material: make(map[string]*UploadMaterial, 50),
 		File:     make(map[string]*UploadFile, 50),
 	}
 	return
