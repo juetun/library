39ec45ff913e60e2d63f61329a71461394d1792f
changjiang <zhaocj2009@126.com>
2023-04-08 17:25:25 +0800

    12

---
+++ common/app_param/tree.go
@@ -0,0 +1,15 @@
+package app_param
+
+type (
+	TreeOption struct {
+		Title           string        `json:"title"`           //标题
+		Expand          bool          `json:"expand"`          //是否展开直子节点
+		Disabled        bool          `json:"disabled"`        //禁掉响应
+		DisableCheckbox bool          `json:"disableCheckbox"` //禁掉 checkbox
+		Selected        bool          `json:"selected"`        //是否选中子节点
+		Checked         bool          `json:"checked"`         //是否勾选(如果勾选，子节点也会全部勾选)
+		Contextmenu     bool          `json:"contextmenu"`     //是否支持右键菜单
+		Indeterminate   bool          `json:"indchk"`          //设置 indeterminate 状态，只负责样式控制 (自定义选择时使用)
+		Children        []*TreeOption `json:"children"`        //子节点属性数组
+	}
+)
