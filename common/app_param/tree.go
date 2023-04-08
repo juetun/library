4f3c1c00ff03b914ebb95a56e03f413dacee8261
changjiang <zhaocj2009@126.com>
2023-04-08 19:14:12 +0800

    12

--- common/app_param/tree.go
+++ common/app_param/tree.go
@@ -9,6 +9,7 @@ type (
 		DisableCheckbox bool          `json:"disableCheckbox"` //禁掉 checkbox
 		Selected        bool          `json:"selected"`        //是否选中子节点
 		Checked         bool          `json:"checked"`         //是否勾选(如果勾选，子节点也会全部勾选)
+		Level           int           `json:"level"`           //级别
 		Contextmenu     bool          `json:"contextmenu"`     //是否支持右键菜单
 		Indeterminate   bool          `json:"indchk"`          //设置 indeterminate 状态，只负责样式控制 (自定义选择时使用)
 		Children        []*TreeOption `json:"children"`        //子节点属性数组
