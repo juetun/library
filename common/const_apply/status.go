29694eda886fd338a355c2277c645f78254e4ac5
changjiang <zhaocj2009@126.com>
2023-05-04 12:24:23 +0800

    12

---
+++ common/const_apply/status.go
@@ -0,0 +1,10 @@
+package const_apply
+
+const (
+	ApplyStatusOk          int8 = iota + 1 //入驻状态审核通过
+	ApplyStatusInvalid                     //已失效
+	ApplyStatusFailure                     //入驻状态审核失败
+	ApplyStatusInit                        //入驻状态初始化或编辑中
+	ApplyStatusAuditing                    //审核中
+	ApplyStatusTimeInvalid                 //超时(失效)
+)
