a768efeb66d72242ca3f26b534f27dd68ceb8ff1
zhaocj <zhaocj2009@126.com>
2023-05-31 09:04:33 +0800

    12

--- common/app_param/order_create/argument.go
+++ common/app_param/order_create/argument.go
@@ -1,7 +1,6 @@
 package order_create
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/juetun/base-wrapper/lib/base"
 	"github.com/juetun/base-wrapper/lib/common"
@@ -13,8 +12,8 @@ type (
 	ArgCreateOrderFromCart struct {
 		RequestUser app_param.RequestUser `json:"-" form:"-"`
 		common.HeaderInfo
-		OrderId            string                            `json:"order_id" form:"order_id"` //订单ID号
-		SkuString          string                            `json:"sku_item,omitempty" form:"sku_item"`
+		OrderId string `json:"order_id" form:"order_id"` //订单ID号
+		//SkuString          string                            `json:"sku_item,omitempty" form:"sku_item"`
 		Amount             string                            `json:"amount,omitempty" form:"amount"`         // 总金额
 		Status             uint8                             `json:"status,omitempty" form:"status"`         // 订单状态
 		AddressId          int64                             `json:"address_id,omitempty" form:"address_id"` // 收货地址
@@ -23,7 +22,7 @@ type (
 		Type               string                            `json:"type,omitempty" form:"Type"`             //数据操作路径
 		ReceiptUserInfo    *ReceiptUserInfo                  `json:"receipt_user_info,omitempty" form:"receipt_user_info"`
 		PriceFreightResult *freight.PriceFreightResult       `json:"freight_result" form:"freight_result"` //订单的邮费计算结果 api-mall服务侧计算
-		SkuItems           []*app_param.ArgOrderFromCartItem `json:"-" form:"-"`
+		SkuItems           []*app_param.ArgOrderFromCartItem `json:"sku_item,omitempty" form:"sku_item"`
 		TimeNow            base.TimeNormal                   `json:"-" form:"-"`
 		GetDataTypeCommon  base.GetDataTypeCommon            `json:"dt_common" form:"dt_common"`
 	}
@@ -59,10 +58,13 @@ func (r *ArgCreateOrderFromCart) GetShopIds() (shopIds []int64) {
 }
 
 func (r *ArgCreateOrderFromCart) Default(c *base.Context) (err error) {
+	if err = r.InitHeaderInfo(c.GinContext); err != nil {
+		return
+	}
 	if err = r.RequestUser.InitRequestUser(c); err != nil {
 		return
 	}
-	
+
 	r.TimeNow = base.GetNowTimeNormal()
 
 	if err = r.validateSku(); err != nil {
@@ -102,23 +104,23 @@ func (r *ArgCreateOrderFromCart) RestSkuStringWithSkuItems() (err error) {
 	if r.SkuItems == nil {
 		return
 	}
-	var bt []byte
-	if bt, err = json.Marshal(r.SkuItems); err != nil {
-		return
-	}
-	r.SkuString = string(bt)
+	//var bt []byte
+	//if bt, err = json.Marshal(r.SkuItems); err != nil {
+	//	return
+	//}
+	//r.SkuString = string(bt)
 	return
 }
 
 func (r *ArgCreateOrderFromCart) validateSku() (err error) {
-	if r.SkuString == "" {
-		err = fmt.Errorf("请选择要付款的商品")
-		return
-	}
-	if err = json.Unmarshal([]byte(r.SkuString), &r.SkuItems); err != nil {
-		err = fmt.Errorf("参数异常")
-		return
-	}
+	//if r.SkuString == "" {
+	//	err = fmt.Errorf("请选择要付款的商品")
+	//	return
+	//}
+	//if err = json.Unmarshal([]byte(r.SkuString), &r.SkuItems); err != nil {
+	//	err = fmt.Errorf("参数异常")
+	//	return
+	//}
 	if len(r.SkuItems) == 0 {
 		err = fmt.Errorf("未选择要付款的商品")
 		return
