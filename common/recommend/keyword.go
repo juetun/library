30b595a1759628d7f2abe5a8950ce194bbff12de
zhaocj <zhaocj2009@126.com>
2023-06-03 15:24:27 +0800

    12

--- common/recommend/keyword.go
+++ common/recommend/keyword.go
@@ -1,11 +1,28 @@
 package recommend
 
 import (
+	"github.com/juetun/base-wrapper/lib/app/app_obj"
 	"github.com/juetun/base-wrapper/lib/base"
 	"github.com/juetun/base-wrapper/lib/common"
+	"github.com/juetun/base-wrapper/lib/common/response"
+	"github.com/juetun/library/common/app_param"
+	"strings"
 )
 
 type (
+	ArgPageSearch struct {
+		response.PageQuery
+		app_param.RequestUser
+		common.HeaderInfo
+		base.GetDataTypeCommon
+		TypeString       string          `json:"type" form:"type"`
+		Types            []string        `json:"-" form:"-"`
+		TimeNow          base.TimeNormal `json:"time_now" form:"time_now"`
+		HeaderInfoString string          `json:"header_info" form:"header_info"`
+	}
+	ResultPageSearch struct {
+		response.Pager
+	}
 	ArgKeywordSave struct {
 		common.HeaderInfo
 		UserHid int64           `json:"user_hid" form:"user_hid"`
@@ -16,6 +33,36 @@ type (
 		Result bool `json:"result"`
 	}
 )
+
+//重置type类型
+func (r *ArgPageSearch) ResetTypes() {
+	r.TypeString = strings.Join(r.Types, ",")
+}
+
+func (r *ArgPageSearch) Default(c *base.Context) (err error) {
+
+	if err = r.InitHeaderInfo(c.GinContext); err != nil {
+		return
+	}
+
+	_ = r.InitRequestUser(c)
+
+	if r.TimeNow.IsZero() {
+		r.TimeNow = base.GetNowTimeNormal()
+	}
+
+	types := strings.Split(r.TypeString, ",")
+	r.Types = make([]string, 0, len(types))
+	for _, item := range types {
+		if item == "" {
+			continue
+		}
+		r.Types = append(r.Types, item)
+	}
+
+	r.HeaderInfoString = c.GinContext.Request.Header.Get(app_obj.HttpHeaderInfo)
+	return
+}
 
 func (r *ArgKeywordSave) Default(ctx *base.Context) (err error) {
 	var (
