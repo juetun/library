9ef21068a192f1680f9ec3e49a078302836e813a
zhaocj <zhaocj2009@126.com>
2023-06-15 11:20:28 +0800

    1

--- common/const_apply/status.go
+++ common/const_apply/status.go
@@ -1,5 +1,7 @@
 package const_apply
 
+import "github.com/juetun/base-wrapper/lib/base"
+
 const (
 	ApplyStatusOk          int8 = iota + 1 //入驻状态审核通过
 	ApplyStatusInvalid                     //已失效
@@ -10,3 +12,20 @@ const (
 	ApplyStatusTimeInvalid                 //超时(失效)
 )
 
+const (
+	InitYes = iota + 1 //初始化
+	InitNo             // 非初始化
+)
+
+var (
+	SliceInit = base.ModelItemOptions{
+		{
+			Label: "初始化",
+			Value: InitYes,
+		},
+		{
+			Label: "非初始化",
+			Value: InitNo,
+		},
+	}
+)
