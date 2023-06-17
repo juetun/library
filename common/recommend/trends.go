c786c8478afc9a05ceca6e6af5e83306b25b00eb
zhaocj <zhaocj2009@126.com>
2023-06-17 09:44:14 +0800

    12

--- common/recommend/trends.go
+++ common/recommend/trends.go
@@ -11,18 +11,38 @@ import (
 	"net/url"
 )
 
+const (
+	TrendContentShowYes uint8 = iota + 1 //可见
+	TrendContentShowNo                   //不可见
+)
+
+var (
+	SliceTrendContentShow = base.ModelItemOptions{
+		{
+			Value: TrendContentShowYes,
+			Label: "可见",
+		},
+		{
+			Value: TrendContentShowNo,
+			Label: "不可见",
+		},
+	}
+)
+
 type (
 	//动态信息
 	TrendContent struct {
-		UserHid   int64           `json:"user_hid"`             //用户信息
-		DataType  string          `json:"data_type"`            //数据类型
-		DataId    string          `json:"data_id"`              //数据ID
-		Img       string          `json:"img,omitempty"`        //头图
-		HaveVideo bool            `json:"have_video,omitempty"` //是否有视频
-		Video     string          `json:"video,omitempty"`      //视频信息
-		Title     string          `json:"title,omitempty"`
-		Desc      string          `json:"desc,omitempty"` //动态内容
-		Time      base.TimeNormal `json:"time"`           //时间
+		UserHid        int64           `json:"user_hid"`             //用户信息
+		DataType       string          `json:"data_type"`            //数据类型
+		DataId         string          `json:"data_id"`              //数据ID
+		Img            string          `json:"img,omitempty"`        //头图
+		HaveVideo      bool            `json:"have_video,omitempty"` //是否有视频
+		Video          string          `json:"video,omitempty"`      //视频信息
+		UserShow       uint8           `json:"user_show"`            //用户是否可见 1-可见 2-不可见
+		AttendUserShow uint8           `json:"attend_user_show"`     //关注用户是否可见 1-可件 2-不可见
+		Title          string          `json:"title,omitempty"`      //动态标题
+		Desc           string          `json:"desc,omitempty"`       //动态内容
+		Time           base.TimeNormal `json:"time"`                 //时间
 	}
 	TrendContents struct {
 		Data []*TrendContent `json:"data"`
@@ -31,6 +51,36 @@ type (
 		Result bool `json:"result"`
 	}
 )
+
+func (r *TrendContent) Default() (res string) {
+	if r.AttendUserShow == 0 {
+		r.AttendUserShow = TrendContentShowYes
+	}
+	if r.UserShow == 0 {
+		r.UserShow = TrendContentShowYes
+	}
+	return
+}
+
+func (r *TrendContent) ParseAttendUserShow() (res string) {
+	mapShow, _ := SliceTrendContentShow.GetMapAsKeyUint8()
+	if tmp, ok := mapShow[r.AttendUserShow]; ok {
+		res = tmp
+		return
+	}
+	res = fmt.Sprintf("未知类型(%v)", r.UserShow)
+	return
+}
+
+func (r *TrendContent) ParseUserShow() (res string) {
+	mapShow, _ := SliceTrendContentShow.GetMapAsKeyUint8()
+	if tmp, ok := mapShow[r.UserShow]; ok {
+		res = tmp
+		return
+	}
+	res = fmt.Sprintf("未知类型(%v)", r.UserShow)
+	return
+}
 
 func (r *TrendContents) GetUserHidAndMap() (userHIds []int64, dataMap map[string][]*TrendContent, err error) {
 	var l = len(r.Data)
@@ -55,7 +105,9 @@ func (r *TrendContents) GetJsonByte() (bytes []byte, err error) {
 }
 
 func (r *TrendContents) Default(ctx *base.Context) (err error) {
-
+	for _, item := range r.Data {
+		item.Default()
+	}
 	return
 }
 
