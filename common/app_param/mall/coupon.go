c77e6faea6a6a58e7c97286fc90bea654b572104
zhaocj <zhaocj2009@126.com>
2023-06-20 12:24:20 +0800

    12

--- common/app_param/mall/coupon.go
+++ common/app_param/mall/coupon.go
@@ -6,6 +6,7 @@ type (
 	ArgGetCouponBindData struct {
 		CouponId string   `json:"coupon_id" form:"coupon_id"`
 		SpuIds   []string `json:"spu_ids" form:"spu_ids"`
+		DataType string   `json:"data_type" form:"data_type"`
 	}
 	ResGetCouponBindData map[string]bool
 )
