38a53d562f470042a0b092e6312f4715090667c5
zhaocj <zhaocj2009@126.com>
2023-06-29 21:30:34 +0800

    12

--- common/app_user/user_browser.go
+++ common/app_user/user_browser.go
@@ -19,6 +19,7 @@ type (
 
 const (
 	UserBrowserCacheNameSpace = "user_browser" //用户浏览计数器缓存的namespace
+	BrowserMaxCount           = 300
 )
 
 //用户浏览缓存的KEY
@@ -36,7 +37,7 @@ func getCtx(ctxs ...context.Context) (ctx context.Context) {
 }
 
 //设置用户的tag标签值
-func SetUserBrowser(ctx *base.Context, userHid int64, dataList []*UserBrowser, ctxs ...context.Context) (err error) {
+func SetUserBrowser(ctx *base.Context, dataList []*UserBrowser, ctxs ...context.Context) (err error) {
 	defer func() {
 		if err == nil || ctx == nil {
 			return
@@ -52,34 +53,43 @@ func SetUserBrowser(ctx *base.Context, userHid int64, dataList []*UserBrowser, c
 	}
 
 	var (
+		l              = len(dataList)
 		cacheClient, _ = app_obj.GetRedisClient(UserBrowserCacheNameSpace)
 		dataItem       *redis.Z
-		data           = make([]*redis.Z, 0, len(dataList))
 		item           *UserBrowser
+		groupData      = make(map[int64][]*redis.Z, l)
 	)
 
 	for _, item = range dataList {
+		if _, ok := groupData[item.UserHid]; !ok {
+			groupData[item.UserHid] = make([]*redis.Z, 0, )
+		}
 		dataItem = &redis.Z{
 			Score:  float64(item.TimeStamp.UnixNano()),
 			Member: item,
 		}
-		data = append(data, dataItem)
+		groupData[item.UserHid] = append(groupData[item.UserHid], dataItem)
 	}
 
 	var (
-		ctxt = getCtx(ctxs...)
-		key  = GetUserBrowserCacheKey(userHid)
+		ctxt     = getCtx(ctxs...)
+		cacheKey string
 	)
 
-	if len(data) > 0 {
-		//添加数据
-		if err = cacheClient.ZAdd(ctxt, key, data...).Err(); err != nil {
-			return
+	if len(groupData) > 0 {
+		for userHid, data := range groupData {
+			cacheKey = GetUserBrowserCacheKey(userHid)
+			//添加数据
+			if err = cacheClient.ZAdd(ctxt, cacheKey, data...).Err(); err != nil {
+				return
+			}
+
+			//移除有序集合中300名以后的数据
+			if err = cacheClient.ZRemRangeByRank(ctxt, cacheKey, BrowserMaxCount, 0).Err(); err != nil {
+				return
+			}
 		}
-		//移除有序集合中300名以后的数据
-		if err = cacheClient.ZRemRangeByRank(ctxt, key, 300, 0).Err(); err != nil {
-			return
-		}
+
 	}
 
 	return
