4be9198fd2b8968bc3d1862675431d1d00cad0bb
zhaocj <zhaocj2009@126.com>
2023-07-03 15:14:19 +0800

    12

--- common/recommend/trends.go
+++ common/recommend/trends.go
@@ -77,7 +77,7 @@ func (r *TrendContent) Default() (err error) {
 		err = fmt.Errorf("请选择动态类型(%v)", r.TrendType)
 		return
 	}
-	if _, ok := app_param.TrendsTypes[r.DataType]; !ok {
+	if _, ok := app_param.TrendsTypes[r.TrendType]; !ok {
 		err = fmt.Errorf("当前服务不支持您选择的动态类型(%v),请在服务初始化时(一般main.go中通过预加载设置)", r.DataType)
 		return
 	}
