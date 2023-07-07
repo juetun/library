c58c06818d4a9d63d182bbade1a332751d952bd0
zhaocj <zhaocj2009@126.com>
2023-07-07 11:23:56 +0800

    12

---
+++ common/app_param/cache_param.go
@@ -0,0 +1,33 @@
+package app_param
+
+import (
+	"github.com/juetun/base-wrapper/lib/base"
+	"github.com/juetun/base-wrapper/lib/common/redis_pkg"
+)
+
+type (
+	ArgClearCacheByKeyPrefix struct {
+		MicroApp  string `json:"micro_app" form:"micro_app"`
+		KeyPrefix string `json:"key_prefix" form:"key_prefix"`
+	}
+
+	ResultClearCacheByKeyPrefix struct {
+		Resutlt bool `json:"resutlt"`
+	}
+
+	ArgGetCacheParamConfig struct {
+		MicroApp string `json:"micro_app" form:"micro_app"`
+	}
+
+	ResultGetCacheParamConfig map[string]redis_pkg.CacheProperty
+)
+
+func (r *ArgClearCacheByKeyPrefix) Default(c *base.Context) (err error) {
+
+	return
+}
+
+func (r *ArgGetCacheParamConfig) Default(c *base.Context) (err error) {
+
+	return
+}
