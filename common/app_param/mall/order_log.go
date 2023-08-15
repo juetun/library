5e337bc6f08fb2021e09f318148c680dd9b6e5f8
zhaocj <zhaocj2009@126.com>
2023-08-15 11:15:25 +0800

    12

--- common/app_param/mall/order_log.go
+++ common/app_param/mall/order_log.go
@@ -1,8 +1,27 @@
 package mall
 
+import "github.com/juetun/base-wrapper/lib/base"
+
 //操作订单日志的用户类型
 const (
 	OrderOperateLogUser = iota + 1 //用户
 	OrderOperateLogShop            //店铺管理员
 	OrderOperateLogPlat            //平台客服
+)
+
+var (
+	SliceOrderOperateLogUserType = base.ModelItemOptions{
+		{
+			Label: "用户",
+			Value: OrderOperateLogUser,
+		},
+		{
+			Label: "店铺管理员",
+			Value: OrderOperateLogShop,
+		},
+		{
+			Label: "平台客服",
+			Value: OrderOperateLogPlat,
+		},
+	}
 )
