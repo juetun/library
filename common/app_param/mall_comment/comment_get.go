6a93a2606c40c8c5ebb28f7a0373ff730acdaed9
zhaocj <zhaocj2009@126.com>
2023-08-26 13:11:04 +0800

    12

--- common/app_param/mall_comment/comment_get.go
+++ common/app_param/mall_comment/comment_get.go
@@ -1,3 +1,20 @@
 package mall_comment
 
+import "github.com/juetun/base-wrapper/lib/base"
 
+type (
+	ArgSpuComment struct {
+		SpuIds            []string               `json:"spu_ids" form:"spu_ids"`
+		GetDataTypeCommon base.GetDataTypeCommon `json:"common"  form:"common"`
+	}
+	ResultSpuComment struct {
+		Code int                      `json:"code"`
+		Data map[string]*OrderComment `json:"data"`
+		Msg  string                   `json:"message"`
+	}
+)
+
+func (r *ArgSpuComment) Default(ctx *base.Context) {
+
+	return
+}
