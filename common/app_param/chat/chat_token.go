e09a976d86ec7c1e1036f56304436eef561dde10
zhaocj <zhaocj2009@126.com>
2023-10-13 12:35:58 +0800

    no message

--- common/app_param/chat/chat_token.go
+++ common/app_param/chat/chat_token.go
@@ -36,7 +36,7 @@ type (
 		FromId     string `json:"from_id" form:"from_id"`
 		ToId       string `json:"to_id" form:"to_id"`             // 聊天用户
 		ToType     uint8  `json:"to_type" form:"to_type"`         // 聊天对象 1-用户 2-店铺 3-聊天室房间
-		NeedAttend int    `json:"need_attend" form:"need_attend"` // 0:不关注 1:关注to_user_hid用户 2:彼此关注
+		NeedAttend uint8  `json:"need_attend" form:"need_attend"` // 0:不关注 1:关注to_user_hid用户 2:彼此关注
 	}
 )
 
