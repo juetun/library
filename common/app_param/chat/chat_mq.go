00b4b915293abd3d0a68b59c85197e2640160ea3
zhaocj <zhaocj2009@126.com>
2023-10-15 11:20:43 +0800

    12

---
+++ common/app_param/chat/chat_mq.go
@@ -0,0 +1,19 @@
+package chat
+
+import "fmt"
+
+const (
+	redisMqTopicTmp = "chat:mq:%v" //
+)
+
+//获取聊天系统监听的队列参数,分布式系统使用
+func GetRedisMqTopicTmp(topicNames ...string) (res string) {
+	var topicName string
+
+	//TODO 当前共用一个，后续可在此实现分桶逻辑
+	//if len(topicNames) > 0 {
+	//	topicName = topicNames[0]
+	//}
+	res = fmt.Sprintf(redisMqTopicTmp, topicName)
+	return
+}
