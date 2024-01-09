73b1ad8d0f8e5139d7afd42390666518059ddedc
zhaocj <zhaocj2009@126.com>
2024-01-09 18:44:42 +0800

    12

--- common/app_param/mall_comment/comment_get.go
+++ common/app_param/mall_comment/comment_get.go
@@ -1,9 +1,13 @@
 package mall_comment
 
-import "github.com/juetun/base-wrapper/lib/base"
+import (
+	"github.com/juetun/base-wrapper/lib/base"
+	"github.com/juetun/base-wrapper/lib/common"
+)
 
 type (
 	ArgSpuComment struct {
+		common.HeaderInfo
 		TopNum            int64                  `json:"top_num" form:"top_num"`
 		SpuIds            []string               `json:"spu_ids" form:"spu_ids"`
 		GetDataTypeCommon base.GetDataTypeCommon `json:"common"  form:"common"`
@@ -14,6 +18,10 @@ type (
 func (r *ArgSpuComment) Default(ctx *base.Context) (err error) {
 	if r.TopNum == 0 {
 		r.TopNum = 2
+	}
+
+	if err = r.InitHeaderInfo(ctx.GinContext); err != nil {
+		return
 	}
 	return
 }
