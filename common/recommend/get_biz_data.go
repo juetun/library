4aa617d59d5b57c306f6b59c3bbcc18c258f60c8
zhaocj <zhaocj2009@126.com>
2024-02-06 23:25:05 +0800

    12

--- common/recommend/get_biz_data.go
+++ common/recommend/get_biz_data.go
@@ -19,7 +19,7 @@ type (
 		DataTypes        string `json:"data_types"`
 		HeaderInfoString string `json:"header_info_string"`
 	}
-	dataStruct struct {
+	DataStructArguments struct {
 		AppName    string     `json:"app_name"`
 		URI        string     `json:"uri"`
 		Method     string     `json:"method"`
@@ -27,46 +27,55 @@ type (
 	}
 )
 
+//获取业务数据的参数配置
+func GetMapDataGetHandler(dataTypes ...string) (res map[string]DataStructArguments) {
+	var dataType string
+	if len(dataTypes) > 0 {
+		dataType = dataTypes[0]
+	}
+	return map[string]DataStructArguments{
+		AdDataDataTypeUserShop: {
+			AppName: app_param.AppNameMall,
+			URI:     "/shop/get_for_recomm",
+			Method:  http.MethodPost, Parameters: url.Values{},
+		}, //配置获取电商数据映射
+		AdDataDataTypeSpu: {
+			AppName: app_param.AppNameMall,
+			URI:     "/product/get_spu_by_ids",
+			Method:  http.MethodPost,
+			Parameters: func() (urlValue url.Values) {
+				urlValue = url.Values{}
+				urlValue.Set("data_types", dataType)
+				return
+			}(),
+		}, //配置获取电商数据映射
+		AdDataDataTypeSocialIntercourse: {
+			AppName: app_param.AppNameSocialIntercourse,
+			URI:     "/data/get_article_by_ids",
+			Method:  http.MethodPost, Parameters: url.Values{},
+		},
+		AdDataDataTypeFishingSport: {
+			AppName: app_param.AppNameSocialIntercourse,
+			URI:     "/data/get_fishing_spots_by_ids",
+			Method:  http.MethodPost, Parameters: url.Values{},
+		},
+		AdDataDataTypeGetSnsData: { //获取社交和钓点数据可使用此参数集中获取
+			AppName: app_param.AppNameSocialIntercourse,
+			URI:     "/data/base_data_by_ids",
+			Method:  http.MethodPost, Parameters: url.Values{},
+		},
+	}
+}
+
 func (r *GetBizData) SyncGetData(groupMapDataId map[string][]string, l int) (res map[string]*DataItem, err error) {
 	res = make(map[string]*DataItem, l)
 
 	var (
-		MapDataGetHandler = map[string]dataStruct{
-			AdDataDataTypeUserShop: {
-				AppName: app_param.AppNameMall,
-				URI:     "/shop/get_for_recomm",
-				Method:  http.MethodPost, Parameters: url.Values{},
-			}, //配置获取电商数据映射
-			AdDataDataTypeSpu: {
-				AppName: app_param.AppNameMall,
-				URI:     "/product/get_spu_by_ids",
-				Method:  http.MethodPost,
-				Parameters: func() (urlValue url.Values) {
-					urlValue = url.Values{}
-					urlValue.Set("data_types", r.DataTypes)
-					return
-				}(),
-			}, //配置获取电商数据映射
-			AdDataDataTypeSocialIntercourse: {
-				AppName: app_param.AppNameSocialIntercourse,
-				URI:     "/data/get_article_by_ids",
-				Method:  http.MethodPost, Parameters: url.Values{},
-			},
-			AdDataDataTypeFishingSport: {
-				AppName: app_param.AppNameSocialIntercourse,
-				URI:     "/data/get_fishing_spots_by_ids",
-				Method:  http.MethodPost, Parameters: url.Values{},
-			},
-			AdDataDataTypeGetSnsData: { //获取社交和钓点数据可使用此参数集中获取
-				AppName: app_param.AppNameSocialIntercourse,
-				URI:     "/data/base_data_by_ids",
-				Method:  http.MethodPost, Parameters: url.Values{},
-			},
-		}
-		dataMul sync.WaitGroup
-		lock    sync.Mutex
-		ok      bool
-		handler dataStruct
+		dataMul           sync.WaitGroup
+		lock              sync.Mutex
+		ok                bool
+		handler           DataStructArguments
+		MapDataGetHandler = GetMapDataGetHandler(r.DataTypes)
 	)
 
 	for key, ids := range groupMapDataId {
@@ -78,7 +87,7 @@ func (r *GetBizData) SyncGetData(groupMapDataId map[string][]string, l int) (res
 		dataMul.Add(1)
 
 		//并行获取商品数据详情
-		go func(bizCode string, idString []string, handlerOp dataStruct) {
+		go func(bizCode string, idString []string, handlerOp DataStructArguments) {
 
 			defer dataMul.Done()
 
