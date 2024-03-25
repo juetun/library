713e1914f6b38bfd9207eca6b645852564229067
zhaocj <zhaocj2009@126.com>
2024-03-25 17:53:49 +0800

    12

--- common/app_param/comment/attend.go
+++ common/app_param/comment/attend.go
@@ -1,17 +1,27 @@
 package comment
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/juetun/base-wrapper/lib/app/app_obj"
 	"github.com/juetun/base-wrapper/lib/base"
+	"github.com/juetun/base-wrapper/lib/plugins/rpc"
+	"github.com/juetun/library/common/app_param"
+	"net/http"
+	"net/url"
 	"strconv"
-	"strings"
 )
 
 type (
 	ArgHasAttendStatus struct {
-		CurrentUid int64   `json:"current_uid" form:"current_uid"`
-		TargetUid  string  `json:"target_uid" form:"target_uid"` //多个用逗号隔开
-		TargetUids []int64 `json:"-" form:"-"`
+		CurrentUid    int64                           `json:"current_uid" form:"current_uid"`
+		TargetUid     []*ArgHasAttendStatusTargetItem `json:"target_uid" form:"target_uid"` //多个用逗号隔开
+		TargetUids    []int64                         `json:"-" form:"-"`
+		TargetShopIds []int64                         `json:"-" form:"-"`
+	}
+	ArgHasAttendStatusTargetItem struct {
+		Type string `json:"type" form:"type"`
+		Id   string `json:"id" form:"id"`
 	}
 	ResultHasAttendStatus map[int64]bool
 )
@@ -19,22 +29,70 @@ type (
 func (r *ArgHasAttendStatus) Default(ctx *base.Context) (err error) {
 	r.TargetUids = make([]int64, 0)
 	var (
-		uid int64
+		id int64
+		l  = len(r.TargetUid)
 	)
-	if r.TargetUid != "" {
-		taUid := strings.Split(r.TargetUid, ",")
-		r.TargetUids = make([]int64, 0, len(taUid))
-		for _, item := range taUid {
-			if uid, err = strconv.ParseInt(item, 10, 64); err != nil {
+	if len(r.TargetUid) > 0 {
+		r.TargetUids = make([]int64, 0, l)
+		for _, item := range r.TargetUid {
+			if id, err = strconv.ParseInt(item.Id, 10, 64); err != nil {
 				err = fmt.Errorf("target_uid参数格式错误")
 				return
 			}
-			if uid == 0 {
+			if id == 0 {
 				continue
 			}
-			r.TargetUids = append(r.TargetUids, uid)
+			switch item.Type {
+			case AttendDataTypeShop:
+				r.TargetShopIds = append(r.TargetShopIds, id)
+			case AttendDataTypeUser:
+				r.TargetUids = append(r.TargetUids, id)
+			default:
+
+			}
 		}
 	}
 
+	return
+}
+
+func (r *ArgHasAttendStatus) ToJson() (res []byte) {
+	res, _ = json.Marshal(r)
+	return
+}
+
+//根据用户ID获取用户信息
+func FetchHasAttendStatus(ctx *base.Context, arg *ArgHasAttendStatus) (res map[string]map[int64]bool, err error) {
+	var (
+		l = len(arg.TargetUids)
+	)
+	res = make(map[string]map[int64]bool, l)
+	if l == 0 {
+		return
+	}
+	var value = url.Values{}
+	ro := rpc.RequestOptions{
+		Method:      http.MethodPost,
+		AppName:     app_param.AppNameComment,
+		URI:         "/act/has_attend",
+		Header:      http.Header{},
+		Value:       value,
+		Context:     ctx,
+		PathVersion: app_obj.App.AppRouterPrefix.Intranet,
+		BodyJson:    arg.ToJson(),
+	}
+	var data = struct {
+		Code int                       `json:"code"`
+		Data map[string]map[int64]bool `json:"data"`
+		Msg  string                    `json:"message"`
+	}{}
+	err = rpc.NewHttpRpc(&ro).
+		Send().
+		GetBody().
+		Bind(&data).Error
+	if err != nil {
+		return
+	}
+	res = data.Data
 	return
 }
