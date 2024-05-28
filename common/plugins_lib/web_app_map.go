d7d93f16dabe429f02124005ead8eb6be2313d67
zhaocj <zhaocj2009@126.com>
2024-05-28 17:48:38 +0800

    12

--- common/plugins_lib/web_app_map.go
+++ common/plugins_lib/web_app_map.go
@@ -25,7 +25,7 @@ func PluginWebMap(arg *app_start.PluginsOperate) (err error) {
 		SystemOutPrintf(fmt.Sprintf("Load webMap config finished \n"))
 
 	var yamlFile []byte
-	var filePath = common.GetConfigFilePath("webmap.yaml")
+	var filePath = common.GetCommonConfigFilePath("webmap.yaml")
 	if yamlFile, err = os.ReadFile(filePath); err != nil {
 		io.SystemOutFatalf("yamlFile.Get err #%v \n", err)
 	}
