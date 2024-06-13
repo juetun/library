9df38842c47e52944e1a8e77adbfa83025e3d9b9
zhaocj <zhaocj2009@126.com>
2024-06-13 19:10:31 +0800

    12

--- common/app_param/mall/order_log.go
+++ common/app_param/mall/order_log.go
@@ -4,9 +4,10 @@ import "github.com/juetun/base-wrapper/lib/base"
 
 //操作订单日志的用户类型
 const (
-	OrderOperateLogUser = iota + 1 //用户
-	OrderOperateLogShop            //店铺管理员
-	OrderOperateLogPlat            //平台客服
+	OrderOperateLogUser   = iota + 1 //用户
+	OrderOperateLogShop              //店铺管理员
+	OrderOperateLogPlat              //平台客服
+	OrderOperateLogSystem            //系统
 )
 
 var (
@@ -22,6 +23,10 @@ var (
 		{
 			Label: "平台客服",
 			Value: OrderOperateLogPlat,
+		},
+		{
+			Label: "系统",
+			Value: OrderOperateLogSystem,
 		},
 	}
 )
