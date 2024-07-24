107230ebf83d4a91b29773a706a47f7ac62fc361
zhaocj <zhaocj2009@126.com>
2024-07-24 16:43:43 +0800

    12

--- common/app_param/app_terminal.go
+++ common/app_param/app_terminal.go
@@ -3,9 +3,9 @@ package app_param
 import "github.com/juetun/base-wrapper/lib/base"
 
 const (
-	ForceUpgradeDefault = iota //不升级 忽略
-	ForceUpgradeDialog         //弹窗提示升级
-	ForceUpgradeYes            //强制用户升级app
+	ForceUpgradeDefault uint8 = iota //不升级 忽略
+	ForceUpgradeDialog               //弹窗提示升级
+	ForceUpgradeYes                  //强制用户升级app
 )
 
 var (
