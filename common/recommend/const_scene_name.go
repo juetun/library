d731c9b5055c693810b312762856a41c5cae10d7
zhaocj <zhaocj2009@126.com>
2024-07-25 16:02:18 +0800

    12

--- common/recommend/const_scene_name.go
+++ common/recommend/const_scene_name.go
@@ -46,8 +46,9 @@ const (
 	AdDataDataTypeRingComment              = "23" //评论 （圈子动态或钓点信息）
 	AdDataDataTypeExpress                  = "24" //物流公司LOGO
 
-	AdDataDataTypeOther  = "200" //其他数据
-	AdDataDataTypeAllSpu = "-1"  //所有的商品
+	AdDataDataTypeAppInstall = "199" //手机APP安装包
+	AdDataDataTypeOther      = "200" //其他数据
+	AdDataDataTypeAllSpu     = "-1"  //所有的商品
 
 )
 
