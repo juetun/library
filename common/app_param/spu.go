0f96570945f3910deb2f8428547705c0961efeae
zhaocj <zhaocj2009@126.com>
2024-11-27 22:56:56 +0800

    12

--- common/app_param/spu.go
+++ common/app_param/spu.go
@@ -1,19 +1,20 @@
 package app_param
 
 const (
-	SpuDataBase                = "spu_data"         //商品信息
-	SpuDataShop                = "spu_shop"         // 店铺信息
-	SpuDataShopExt             = "spu_shop_ext"     // 店铺扩展信息
-	SpuDataShopNotice          = "spu_shop_notice"  // 店铺公告信息
-	SpuDataBrand               = "spu_brand"        // 品牌信息
-	SpuDataTypeProductDesc     = "spu_product_desc" // 商品描述
-	SpuDataTypeSKus            = "spu_sku"          // SKU信息
-	SpuDataTypeSKusGift        = "spu_gifts"        // 赠品信息
-	SpuDataTypeSKusStock       = "spu_sku_stock"    // sku库存
-	SpuDataTypeSKusRelate      = "spu_sku_relate"   // sku关联属性
-	SpuDataTypeSKusProperty    = "spu_sku_property" // sku属性
-	SpuDataTypeFreightTemplate = "spu_freight"      // 运费模板
-	SpuDataTypeComment         = "comment"          // 评论信息（推荐或最近评论的信息）
+	SpuDataBase                = "spu_data"          // 商品信息
+	SpuDataShop                = "spu_shop"          // 店铺信息
+	SpuDataShopExt             = "spu_shop_ext"      // 店铺扩展信息
+	SpuDataShopNotice          = "spu_shop_notice"   // 店铺公告信息
+	SpuDataBrand               = "spu_brand"         // 品牌信息
+	SpuDataTypeProductDesc     = "spu_product_desc"  // 商品描述
+	SpuActivity                = "spu_plat_activity" //商品加入平台活动信息
+	SpuDataTypeSKus            = "spu_sku"           // SKU信息
+	SpuDataTypeSKusGift        = "spu_gifts"         // 赠品信息
+	SpuDataTypeSKusStock       = "spu_sku_stock"     // sku库存
+	SpuDataTypeSKusRelate      = "spu_sku_relate"    // sku关联属性
+	SpuDataTypeSKusProperty    = "spu_sku_property"  // sku属性
+	SpuDataTypeFreightTemplate = "spu_freight"       // 运费模板
+	SpuDataTypeComment         = "comment"           // 评论信息（推荐或最近评论的信息）
 	/*****************************上传信息获取**********************************/
 	//SpuUploadAll          = "spu_img"         // 商品图片获取所有图片 (包括SpuImg前缀的属性如:SpuUploadImgThumbnail,SpuUploadImgPic,SpuSkuImg,SpuUploadDescription）
 	SpuUploadImgThumbnail = "spu_img_thumb"   // 商品缩略图
@@ -25,4 +26,3 @@ const (
 	SpuShopLogo           = "spu_shop_logo"   // 店铺LOGO
 	SpuShopNoticeImg      = "spu_notice_img"  // 店铺公告图片
 )
-
