aac6532a2bf55949d8eb151fed640e44421d0ab7
zhaocj <zhaocj2009@126.com>
2025-02-11 20:27:45 +0800

    12

--- common/app_param/mall/models/plat_activity.go
+++ common/app_param/mall/models/plat_activity.go
@@ -2,7 +2,13 @@ package models
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/juetun/base-wrapper/lib/app/app_obj"
 	"github.com/juetun/base-wrapper/lib/base"
+	"github.com/juetun/base-wrapper/lib/plugins/rpc"
+	"github.com/juetun/library/common/app_param"
+	"net/http"
+	"net/url"
 	"time"
 )
 
@@ -14,8 +20,9 @@ const (
 	PlatActivityDataTypeSpu uint8 = iota + 1 //平台活动数据来源
 )
 const (
-	PlatActivityBindDataTypeCoupon  uint8 = iota + 1 //活动绑定优惠券
-	PlatActivityBindDataTypeAtPrice                  //指定价个购买
+	PlatActivityNone                uint8 = iota //未参加活动
+	PlatActivityBindDataTypeCoupon               //活动绑定优惠券
+	PlatActivityBindDataTypeAtPrice              //指定价个购买
 )
 const (
 	PlatActivityStatusUse        uint8 = iota + 1 //有效
@@ -80,6 +87,10 @@ var (
 	//活动绑定数据（如优惠券 、代金券、1元购 0元购等）
 	PlatActivityBindDataType = base.ModelItemOptions{
 		{
+			Label: "",
+			Value: PlatActivityNone,
+		},
+		{
 			Label: "绑定优惠券",
 			Value: PlatActivityBindDataTypeCoupon,
 		},
@@ -91,6 +102,10 @@ var (
 )
 
 type (
+	//商品活动
+	ArgPlatActivityByIds struct {
+		base.ArgGetByStringIds
+	}
 	PlatActivity struct {
 		Id           int64            `json:"id"`
 		Title        string           `json:"title"`
@@ -102,10 +117,27 @@ type (
 		StartTime    *base.TimeNormal `json:"start_time"`
 		OverTime     *base.TimeNormal `json:"over_time"`
 	}
+
+
 	OtherAttrValue struct {
 		Price string `json:"price"` //价格
 	}
 )
+
+func (r *PlatActivity) GetOtherAttr() (otherAttrValue *OtherAttrValue, err error) {
+	otherAttrValue = &OtherAttrValue{}
+	defer func() {
+		if err != nil {
+			return
+		}
+		err = otherAttrValue.Default()
+	}()
+	if r.OtherAttr == "" {
+		return
+	}
+	err = json.Unmarshal([]byte(r.OtherAttr), otherAttrValue)
+	return
+}
 
 func (r *PlatActivity) InitOtherAttr(price string, otherAttrValues ...*OtherAttrValue) (err error) {
 	var otherAttrValue *OtherAttrValue
@@ -128,9 +160,69 @@ func (r *PlatActivity) InitOtherAttr(price string, otherAttrValues ...*OtherAttr
 	r.OtherAttr = string(bt)
 	return
 }
+
+
 func (r *OtherAttrValue) Default() (err error) {
 	if r.Price == "" {
 		r.Price = "0.00"
 	}
+	return
+}
+
+
+
+
+
+func (r *ArgPlatActivityByIds) Default(ctx *base.Context) (err error) {
+
+	return
+}
+
+//获取平台活动信息
+func GetPlatActivity(ctx *base.Context, args *ArgPlatActivityByIds) (res map[string]*PlatActivity, err error) {
+	if len(args.Ids) == 0 {
+		return
+	}
+	arg := url.Values{}
+	params := rpc.RequestOptions{
+		Context:     ctx,
+		Method:      http.MethodPost,
+		AppName:     app_param.AppNameMallActivity,
+		URI:         "/plat_activity/get_by_ids",
+		Value:       arg,
+		PathVersion: app_obj.App.AppRouterPrefix.Intranet,
+		Header:      http.Header{},
+	}
+
+	if ctx.GinContext != nil {
+		params.Header.Set(app_obj.HttpHeaderInfo, ctx.GinContext.GetHeader(app_obj.HttpHeaderInfo))
+	}
+	if params.BodyJson, err = json.Marshal(args); err != nil {
+		return
+	}
+
+	req := rpc.NewHttpRpc(&params).
+		Send().GetBody()
+	if err = req.Error; err != nil {
+		return
+	}
+	var body []byte
+	if body = req.Body; len(body) == 0 {
+		return
+	}
+
+	var resResult struct {
+		Code int                              `json:"code"`
+		Data map[string]*PlatActivity `json:"data"`
+		Msg  string                           `json:"message"`
+	}
+	if err = json.Unmarshal(body, &resResult); err != nil {
+		return
+	}
+	if resResult.Code > 0 {
+		err = fmt.Errorf(resResult.Msg)
+		return
+	}
+	res = resResult.Data
 	return
 }
