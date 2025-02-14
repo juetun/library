d9db1c3b499b55b3a053e4e37f6b1e2f515952a8
zhaocj <zhaocj2009@126.com>
2025-02-14 15:48:31 +0800

    12

---
+++ common/app_param/sns/sns.go
@@ -0,0 +1,31 @@
+package sns
+
+import (
+	"github.com/juetun/base-wrapper/lib/base"
+	"github.com/juetun/base-wrapper/lib/common"
+	"github.com/juetun/library/common/app_param"
+)
+
+type (
+	ArgRingsId struct {
+		common.HeaderInfo
+		app_param.RequestUser
+		base.ArgGetByNumberIds
+	}
+	ResultRingMap  map[string]ResultRingItem
+	ResultRingItem struct {
+		ID           int64  `json:"id"`
+		Title        string `json:"title,omitempty"`
+		Thumbnail    string `json:"thumbnail,omitempty"`
+		ThumbnailURL string `json:"thumbnail_url"`
+		ShowLevel    uint8  `json:"show_level"`
+		IsRecommend  uint8  `json:"is_recommend"` //是否推荐圈子
+		Status       int8   `json:"status,omitempty"`
+		UserHid      int64  `json:"user_hid"`
+	}
+)
+
+func (r *ArgRingsId) Default(ctx *base.Context) (err error) {
+
+	return
+}
