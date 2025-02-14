baa06ca9ab0ea533a1903078c548b51c1f399213
zhaocj <zhaocj2009@126.com>
2025-02-14 17:26:43 +0800

    12

--- common/app_param/sns/sns.go
+++ common/app_param/sns/sns.go
@@ -14,18 +14,19 @@ type (
 	}
 	ResultRingMap  map[string]ResultRingItem
 	ResultRingItem struct {
-		ID           int64  `json:"id"`
-		Title        string `json:"title,omitempty"`
-		Thumbnail    string `json:"thumbnail,omitempty"`
-		ThumbnailURL string `json:"thumbnail_url"`
-		ShowLevel    uint8  `json:"show_level"`
-		IsRecommend  uint8  `json:"is_recommend"` //是否推荐圈子
-		Key          string `json:"key,omitempty"`
-		PingYin      string `json:"ping_yin,omitempty"`
-		PingYinFirst string `json:"ping_yin_first,omitempty"`
-		Description  string `json:"description"`
-		Status       int8   `json:"status,omitempty"`
-		UserHid      int64  `json:"user_hid"`
+		ID             int64  `json:"id"`
+		Title          string `json:"title,omitempty"`
+		Thumbnail      string `json:"thumbnail,omitempty"`
+		ThumbnailURL   string `json:"thumbnail_url"`
+		ShowLevel      uint8  `json:"show_level"`
+		IsRecommend    uint8  `json:"is_recommend"` //是否推荐圈子
+		Key            string `json:"key,omitempty"`
+		PingYin        string `json:"ping_yin,omitempty"`
+		PingYinFirst   string `json:"ping_yin_first,omitempty"`
+		Description    string `json:"description"`
+		SubDescription string `json:"sub_description"`
+		Status         int8   `json:"status,omitempty"`
+		UserHid        int64  `json:"user_hid"`
 	}
 )
 
