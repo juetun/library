a3c2e068e6cc7663028c47d0c70215c97464b571
zhaocj <zhaocj2009@126.com>
2025-03-12 22:55:32 +0800

    12

--- common/app_param/mall/stock.go
+++ common/app_param/mall/stock.go
@@ -32,6 +32,7 @@ type (
 	StockOperateResultItem struct {
 		SpuId        string `json:"spu_id"`
 		SkuId        string `json:"sku_id"`
+		SrcPk        string `json:"src_pk"`        //源SKU的PK值
 		Num          int64  `json:"num"`           //需要加上或减去的库存数
 		ActualityNum int64  `json:"actuality_num"` //实际扣减库存数
 		NotForce     bool   `json:"not_force"`     //非强制扣减或加上库存 （false-强制减库存, true-非强制减，扣到0即止 定金预售功能使用）
