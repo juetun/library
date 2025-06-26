4d8393de46d3ffc745e1b4762117904c4b268579
zhaocj <zhaocj2009@126.com>
2025-06-26 14:30:10 +0800

    12

--- common/app_param/chat_const/chat_send.go
+++ common/app_param/chat_const/chat_send.go
@@ -18,20 +18,20 @@ import (
 
 type (
 	SendMsgChat struct {
-		MsgId           string `json:"msg_id,omitempty" form:"msg_id"`                     // 消息ID
-		ChatType        uint8  `json:"chat_type,omitempty" form:"chat_type"`               // 聊天类型 单聊 、群聊
-		FromId          string `json:"from_id,omitempty" form:"from_id"`                   // 发送消息方
-		FromType        uint8  `json:"from_type,omitempty" form:"from_type"`               //
-		ToId            string `json:"to_id,omitempty" form:"to_id"`                       // 发送给的目标用户或群
-		ToType          uint8  `json:"to_type,omitempty" form:"to_type"`                   //
-		ManagerUserHid  string `json:"manager_user_hid,omitempty" form:"manager_user_hid"` // 管理员ID
-		Content         string `json:"content,omitempty" form:"content"`                   // 消息内容
-		IsCustomer      bool   `json:"is_customer,omitempty" form:"is_customer"`           // 是否是客服聊天
-		CustomerType    uint8  `json:"customer_type,omitempty" form:"customer_type"`       // 客服主体(CustomerManagerDataTypeShop-店铺实体  CustomerManagerDataTypePlatform-系统客服)
-		ToCustomerUser  string `json:"to_customer_user,omitempty" form:"to_customer_user"` // 聊天信息产生时的管理员用户ID
-		CurrentUserRole string `json:"current_user_role" form:"current_user_role"`         //当前用户角色
-		SendType        uint8  `json:"send_type,omitempty" form:"send_type"`               // 发送方式 1-全双工 2-发送方可见 3-接收方可见
-		ReplayType      uint8  `json:"replay_type,omitempty" form:"replay_type"`           //客户端响应数据类型 ，提醒客户端刷新消息内容
+		MsgId           string `json:"msg_id,omitempty" form:"msg_id"`                       // 消息ID
+		ChatType        uint8  `json:"chat_type,omitempty" form:"chat_type"`                 // 聊天类型 单聊 、群聊
+		FromId          string `json:"from_id,omitempty" form:"from_id"`                     // 发送消息方
+		FromType        uint8  `json:"from_type,omitempty" form:"from_type"`                 //
+		ToId            string `json:"to_id,omitempty" form:"to_id"`                         // 发送给的目标用户或群
+		ToType          uint8  `json:"to_type,omitempty" form:"to_type"`                     //
+		ManagerUserHid  string `json:"manager_user_hid,omitempty" form:"manager_user_hid"`   // 管理员ID
+		Content         string `json:"content,omitempty" form:"content"`                     // 消息内容
+		IsCustomer      bool   `json:"is_customer,omitempty" form:"is_customer"`             // 是否是客服聊天
+		CustomerType    uint8  `json:"customer_type,omitempty" form:"customer_type"`         // 客服主体(CustomerManagerDataTypeShop-店铺实体  CustomerManagerDataTypePlatform-系统客服)
+		ToCustomerUser  string `json:"to_customer_user,omitempty" form:"to_customer_user"`   // 聊天信息产生时的管理员用户ID
+		CurrentUserRole string `json:"current_user_role,omitempty" form:"current_user_role"` //当前用户角色
+		SendType        uint8  `json:"send_type,omitempty" form:"send_type"`                 // 发送方式 1-全双工 2-发送方可见 3-接收方可见
+		ReplayType      uint8  `json:"replay_type,omitempty" form:"replay_type"`             //客户端响应数据类型 ，提醒客户端刷新消息内容
 	}
 	ArgGetChatToken struct {
 		ArgGetChatTokenOther
