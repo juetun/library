9a2a6d63a84492cf209f71d8d65ad3ef61521f54
zhaocj <zhaocj2009@126.com>
2025-08-13 15:36:21 +0800

    12

---
+++ common/export_common/public_subscribe.go
@@ -0,0 +1,47 @@
+package export_common
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"github.com/juetun/base-wrapper/lib/app/app_obj"
+	"github.com/juetun/base-wrapper/lib/base"
+)
+
+const (
+	RedisMqTopicTmp = "export:mq:%v" //
+)
+
+const (
+	RedisExportNameSpace = "export"
+)
+
+//获取聊天系统监听的队列参数,分布式系统使用
+func GetRedisMqTopicTmp(topicNames ...string) (res string) {
+	var topicName string
+
+	//TODO 当前共用一个，后续可在此实现分桶逻辑
+	//if len(topicNames) > 0 {
+	//	topicName = topicNames[0]
+	//}
+	res = fmt.Sprintf(RedisMqTopicTmp, topicName)
+	return
+}
+
+type DaoChatCommon struct {
+	base.ServiceDao
+	ctx context.Context
+}
+
+func (r *DaoChatCommon) Init(ctx ...*base.Context) {
+	r.SetContext(ctx...)
+	if r.ctx == nil {
+		r.ctx = context.TODO()
+	}
+	return
+}
+
+func (r *DaoChatCommon) GetExportCacheClient() (cacheClient *redis.Client) {
+	cacheClient, _ = app_obj.GetRedisClient(RedisExportNameSpace)
+	return
+}
