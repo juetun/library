ffc4bf42d317e0ebf66429320bb7fbb79d3fd563
zhaocj <zhaocj2009@126.com>
2025-10-28 17:48:25 +0800

    12

--- common/app_param/queue.go
+++ common/app_param/queue.go
@@ -4,13 +4,13 @@ import "github.com/juetun/base-wrapper/lib/base"
 
 type (
 	ArgAddQueueMessage struct {
-		Data []*MessageQueueData
+		TimeNow base.TimeNormal     `json:"time_now" form:"time_now"`
+		Data    []*MessageQueueData `json:"data" form:"data"`
 	}
 	MessageQueueData struct {
 		Topic     string          `json:"topic" form:"topic"`
 		MessageId string          `json:"message_id" form:"message_id"`
 		Data      string          `json:"data" form:"data"`
-		CreatedAt base.TimeNormal `json:"created_at" form:"created_at"`
 		OnlineAt  base.TimeNormal `json:"online_at" form:"online_at"`
 	}
 	ResultAddQueueMessage struct {
