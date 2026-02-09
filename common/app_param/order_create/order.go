7d33b7a9731d871d9b8a3d1ec506115cedf431a8
zhaocj <zhaocj2009@126.com>
2026-02-09 17:55:31 +0800

    12

--- common/app_param/order_create/order.go
+++ common/app_param/order_create/order.go
@@ -254,6 +254,8 @@ type (
 	}
 	OrderUponOption struct {
 		OrderId   string `json:"order_id"`   //订单号
+		SkuId     string `json:"sku_id"`     //商品ID
+		SpuId     string `json:"spu_id"`     //商品ID
 		Category  string `json:"category"`   //类型
 		SrcPrice  string `json:"src_price"`  //源价
 		DecrPrice string `json:"decr_price"` //扣减价
