0d3f7af967b4582e796865604bc2eb61b091fee4
officialjuetun <zhaocj2009@126.com>
2026-05-12 11:24:45 +0800

    12

--- common/app_user/trend.go
+++ common/app_user/trend.go
@@ -1,6 +1,18 @@
 package app_user
 
-import "github.com/juetun/base-wrapper/lib/base"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/juetun/base-wrapper/lib/app/app_obj"
+	"github.com/juetun/base-wrapper/lib/base"
+	"github.com/juetun/base-wrapper/lib/common"
+	"github.com/juetun/base-wrapper/lib/common/response"
+	"github.com/juetun/base-wrapper/lib/plugins/rpc"
+	"github.com/juetun/library/common/app_param"
+	"github.com/juetun/library/common/recommend"
+	"net/http"
+	"net/url"
+)
 
 const (
 	TrendTypeShopInfo     = "shop_info"     //店铺信息
@@ -49,3 +61,70 @@ var (
 		},
 	}
 )
+
+type (
+	ArgRecommendAttendUser struct {
+		app_param.RequestUser
+		common.HeaderInfo
+		response.PageQuery
+		TimeNow           base.TimeNormal        `json:"-" form:"-"`
+		GetDataTypeCommon base.GetDataTypeCommon `json:"-" form:"-"`
+	}
+)
+
+//根据场景Key获取数据
+func GetUserRecommendData(arg *ArgRecommendAttendUser, headerInfoString string, ctx *base.Context) (res *recommend.PagerRecommend, err error) {
+
+	var header = http.Header{}
+	res = &recommend.PagerRecommend{}
+	header.Set(app_obj.HttpHeaderInfo, headerInfoString)
+	header.Set(app_obj.HttpUserHid, fmt.Sprintf("%v", arg.UUserHid))
+	ro := rpc.RequestOptions{
+		Method:      http.MethodPost,
+		AppName:     app_param.AppNameUser,
+		URI:         "/user/recommend_attend_user",
+		Header:      header,
+		Value:       url.Values{},
+		Context:     ctx,
+		PathVersion: app_obj.App.AppRouterPrefix.Intranet,
+	}
+
+	if ro.BodyJson, err = arg.GetJson(); err != nil {
+		return
+	}
+	var data = struct {
+		Code int `json:"code"`
+		Data struct {
+			Pager struct {
+				List          []recommend.DataItem     `json:"list"`
+				TotalCount    int64                    `json:"total_count,omitempty"`
+				IsNext        bool                     `json:"is_next,omitempty"` // [bool] 是否有下一页，true=有下一页；false=无下页，可关闭列表
+				SceneProperty *recommend.SceneProperty `json:"scene_property"`
+				AdCount       int                      `json:"ad_count,omitempty"` //广告推流数量
+				response.PageQuery
+			} `pager`
+		} `json:"data"`
+		Msg string `json:"message"`
+	}{}
+
+	if err = rpc.NewHttpRpc(&ro).
+		Send().
+		GetBody().
+		Bind(&data).Error; err != nil {
+		return
+	}
+	res.List = data.Data.Pager.List
+	return
+}
+
+func (r *ArgRecommendAttendUser) Default(ctx *base.Context) (err error) {
+	_ = r.InitRequestUser(ctx)
+	_ = r.InitHeaderInfo(ctx.GinContext)
+	r.TimeNow = base.GetNowTimeNormal()
+	return
+}
+
+func (r *ArgRecommendAttendUser) GetJson() (res []byte, err error) {
+	res, err = json.Marshal(r)
+	return
+}
